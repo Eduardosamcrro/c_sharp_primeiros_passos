e7e2fcb5a449036a2d3b1c5ada5ad688531f9d19
Eduardo <dudu48leste@gmail.com>
2022-05-27 22:01:28 -0300

    Adicionar arquivos de projeto.

---
+++ c/Program.cs
@@ -0,0 +1,10 @@
+﻿var nome = "eduardo";
+Console.WriteLine(nome);
+
+Console.WriteLine();
+
+if (nome == "Eduardo")
+{
+    Console.WriteLine(nome);
+}
+else { Console.WriteLine("Falsa igualdade"); }
